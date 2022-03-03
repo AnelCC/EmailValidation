f1b83d8429f69e01f10f3769276a855a5c44ad3a
AnelCC <anel.calvo.cervantes@gmail.com>
2022-03-02 22:18:43 -0500

    View model and user case comunication

--- app/src/main/java/com/anelcc/cleanarchitecturehandleerrors/presentation/BaseViewMode.kt
+++ app/src/main/java/com/anelcc/cleanarchitecturehandleerrors/presentation/BaseViewMode.kt
@@ -3,8 +3,13 @@ package com.anelcc.cleanarchitecturehandleerrors.presentation
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.anelcc.cleanarchitecturehandleerrors.domain.SubmitEmailUseCase
+import com.anelcc.cleanarchitecturehandleerrors.util.Resource
+import kotlinx.coroutines.launch
 
-class BaseViewMode {
+class BaseViewMode(val submitEmailUseCase: SubmitEmailUseCase = SubmitEmailUseCase()) : ViewModel() {
 
     var email by mutableStateOf("")
         private set
@@ -15,5 +20,19 @@ class BaseViewMode {
 
     fun onEmailChange(email: String) {
         this.email = email
+    }
+
+    fun submitEmail() {
+        viewModelScope.launch {
+            val result = submitEmailUseCase.execute(email)
+            when(result) {
+                is Resource.Success -> {
+
+                }
+                is Resource.Error -> {
+                    error = result.message
+                }
+            }
+        }
     }
 }
